e1379840d68b2f1639f3752c7bda846eb7fbbcef
Jonathon <jonnygrealish64@gmail.com>
2021-12-17 15:27:19 +0000

    added values into database

--- playstation.sql
+++ playstation.sql
@@ -1,5 +1,3 @@
-use videogamecollection;
-
 create table games(
     title varchar(250) PRIMARY KEY,
     console varchar(250),
@@ -7,4 +5,8 @@ create table games(
     developer varchar(250)
 );
 
-insert into games (title, console, year, developer) values ();
+insert into games (title, console, year, developer) values ('Uncharted 2: Among Thieves', 'PlayStation 3', 2009, 'Naughty Dog');
+insert into games (title, console, year, developer) values ('Red Dead Redemption 2', 'PlayStation 4', 2019, 'Rockstar');
+insert into games (title, console, year, developer) values ('Yakuza: Like A Dragon', 'PlayStation 5', 2021, 'Ryu Ga Gotoku');
+insert into games (title, console, year, developer) values ('Supermarket Shriek', 'PlayStation 4', 2020, 'Billy Goat Entertainment Ltd');
+insert into games (title, console, year, developer) values ('F1 2021', 'PlayStation 5', 2021, 'Codemasters');
